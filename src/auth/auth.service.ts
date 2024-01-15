84b0132fd708b47d585c197c149d371122d6c618
PlugaruRoman <97292989+PlugaruRoman@users.noreply.github.com>
2024-01-15 22:10:21 +0200

    feat: add login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,33 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from 'src/types/types';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string) {
     const user = await this.usersService.findOne(email);
     const passwordIsMatch = await argon2.verify(user.password, password);
+
     if (user && passwordIsMatch) {
       return user;
     }
     throw new UnauthorizedException('User or Password are incorrect!');
+  }
+
+  async login(user: IUser) {
+    const { id, email } = user;
+
+    return {
+      id,
+      email,
+      token: this.jwtService.sign({ id: user.id, email: user.email }),
+    };
   }
 }
